c85977be623c9500fb78f1f4a4c952b7baf2d72d
Alvin Tang <alvin.tang@anu.edu.au>
2024-04-06 16:38:06 +1100

    Recursive indexation

--- client.c
+++ client.c
@@ -22,7 +22,7 @@ typedef struct item {
 } item;
 
 /* Helper functions */
-void gopher_connect(char *request);
+void gopher_connect(char *hostname, int port, char *request);
 void index(char *path);
 void add_item(item *new_item);
 
@@ -30,6 +30,7 @@ void add_item(item *new_item);
 int fd;                          // Socket file descriptor
 struct sockaddr_in server_addr;  // Address and port information
 struct item *list = NULL;        // Linked list of indexed directories and files
+struct item *last_node = NULL;   // Last item in the linked list
 
 /**
  * The Internet Gopher client indexing files.
@@ -43,6 +44,30 @@ int main(int argc, char* argv[]) {
     char *hostname = argv[1];
     int port = atoi(argv[2]);
     
+    // Begin the indexing process, starting with the root directory
+    gopher_connect(hostname, port, "\r\n");
+
+    // Go through the linked list of indexed items and index subdirectories
+    item *c = list;
+    while (c != NULL) {
+        if (c->item_type == DIRECTORY) {
+            int path_len = strlen(c->path);
+            char *request = malloc(path_len + 3);
+            strcpy(request, c->path);
+            request[path_len] = '\r';
+            request[path_len + 1] = '\n';
+            request[path_len + 2] = '\0';
+            gopher_connect(hostname, port, request);
+        }
+        c = c->next;
+    }
+
+    // Analyse the information of the indexed items and print info
+
+    return 0;
+}
+
+void gopher_connect(char *hostname, int port, char *request) {
     // Create the socket
     fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1) {
@@ -58,31 +83,17 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    // Begin the indexing process, starting with the root directory
-    gopher_connect("\r\n");
-
-    return 0;
-}
-
-void gopher_connect(char *request) {
     // Initiate the connection
     int connect_status = connect(fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if (connect_status == -1) {
         fprintf(stderr, "Error: Connection failed\n");
         exit(1);
     }
-    printf("Connected to server!\n");
 
     index(request);
 
     // Terminate the connection and release the socket
     close(fd);
-
-    item *c = list;
-    while (c != NULL) {
-        fprintf(stdout, "Visit: %s of type %d\n", c->path, c->item_type);
-        c = c->next;
-    }
 }
 
 void index(char *request) {
@@ -91,8 +102,8 @@ void index(char *request) {
     int bytes_received;
 
     // Send a request for the directory index to the server
-    send(fd, request, strlen(request), 0);
-    fprintf(stdout, "Sending request: %s", request);
+    int bytes = send(fd, request, strlen(request), 0);
+    fprintf(stdout, "Sending request: %s", bytes == 2 ? "Root directory\n" : request);
 
     // Read the directory index from the server
     while ((bytes_received = recv(fd, buffer, BUFFER_SIZE, 0)) > 0) {
@@ -127,16 +138,19 @@ void index(char *request) {
 
 /**
  * Add a new item to the linked list of indexed items.
+ * Requires O(1) time.
  * 
  * @param new_item pointer to the new indexed item
  */
 void add_item(item *new_item) {
+    // If the linked list is empty, let the new item be the initial item
     if (list == NULL) {
         list = new_item;
+        last_node = new_item;
         return;
     }
-    
-    item *current_head = list;
-    new_item->next = current_head;
-    list = new_item;
+
+    // Otherwise, append the new item to the end of the linked list
+    last_node->next = new_item;
+    last_node = new_item;
 }
