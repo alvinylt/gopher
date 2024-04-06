02ef901a63c993bdbb6055e36b4af7ed1a8ea6ec
Alvin Tang <alvin.tang@anu.edu.au>
2024-04-06 16:12:23 +1100

    Implement linked list

--- client.c
+++ client.c
@@ -5,16 +5,31 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-// Global constant: string buffer size
+/*  Global constant: string buffer size */
 #define BUFFER_SIZE 1024
 
-// Helper functions
+/* Global constant: file types */
+#define DIRECTORY 0  // Directory
+#define TEXT 1       // Text (non-binary) file
+#define BINARY 2     // Binary file
+
+/* Struct containing information of an indexed directory/file */
+typedef struct item {
+    char path[BUFFER_SIZE];
+    int item_type;
+    int item_size;
+    struct item *next;
+} item;
+
+/* Helper functions */
 void gopher_connect(char *request);
 void index(char *path);
+void add_item(item *new_item);
 
-// Global variables: values used across all functions
+/* Global variables: values used across all functions */
 int fd;                          // Socket file descriptor
 struct sockaddr_in server_addr;  // Address and port information
+struct item *list = NULL;        // Linked list of indexed directories and files
 
 /**
  * The Internet Gopher client indexing files.
@@ -43,7 +58,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    // Interact with the server
+    // Begin the indexing process, starting with the root directory
     gopher_connect("\r\n");
 
     return 0;
@@ -62,6 +77,12 @@ void gopher_connect(char *request) {
 
     // Terminate the connection and release the socket
     close(fd);
+
+    item *c = list;
+    while (c != NULL) {
+        fprintf(stdout, "Visit: %s of type %d\n", c->path, c->item_type);
+        c = c->next;
+    }
 }
 
 void index(char *request) {
@@ -71,26 +92,51 @@ void index(char *request) {
 
     // Send a request for the directory index to the server
     send(fd, request, strlen(request), 0);
-    fprintf(stdout, "Sending request: %s", "\r\n");
+    fprintf(stdout, "Sending request: %s", request);
 
     // Read the directory index from the server
     while ((bytes_received = recv(fd, buffer, BUFFER_SIZE, 0)) > 0) {
         buffer[bytes_received] = '\0';
         char *line = strtok(buffer, "\t\r\n");
-        int item = 0;
+        // Each line in the response contains four columns, separated by tab
+        int column = 0;
+        int item_type;
         while (line != NULL) {
-            if (item == 0) {
-                // Index subdirectory recursively
-                if (line[0] == '1') {
-                    fprintf(stdout, "Directory found: ");
-                }
+            if (column == 0) {
+                // Determine the type of the indexed item
+                if (line[0] == '1') item_type = DIRECTORY;
+                else if (line[0] == '0') item_type = TEXT;
+                else item_type = BINARY;
             }
-            if (item == 1)
-                fprintf(stdout, "%s\n", line);
+            if (column == 1) {
+                fprintf(stdout, "Indexed: %s\n", line);
+                item *new_item = (item *)malloc(sizeof(item));
+                strcpy(new_item->path, line);
+                new_item->item_type = item_type;
+                new_item->next = NULL;
+                add_item(new_item);
+            }
+            // Read the next column in the line or the next line
             line = strtok(NULL, "\t\r\n");
-            item = (item + 1) % 4;
+            column = (column + 1) % 4;
         }
     }
     
     free(buffer);
+}
+
+/**
+ * Add a new item to the linked list of indexed items.
+ * 
+ * @param new_item pointer to the new indexed item
+ */
+void add_item(item *new_item) {
+    if (list == NULL) {
+        list = new_item;
+        return;
+    }
+    
+    item *current_head = list;
+    new_item->next = current_head;
+    list = new_item;
 }
